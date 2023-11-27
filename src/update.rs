0d11faa7e3dbdf88a59c459f2fd14259b9fcd40e
seawal <sean@SWs-MacBook-Air.local>
2023-11-27 18:17:35 +0000

    created basic update fn, add implementation later

---
+++ src/update.rs
@@ -0,0 +1,23 @@
+use crossterm::event::{KeyCode, KeyEvent};
+
+use crate::app::AppState;
+
+// TODO: remove comments after implementing
+// TODO: move this to app.rs 
+// TODO: make part of AppState impementation?
+pub fn update(app: &mut AppState, key_event: KeyEvent) {
+    match key_event.code {
+        // close the progrem
+        KeyCode::Esc | KeyCode::Char('q')=> {},
+        // if highlighting a note, open the editor
+        KeyCode::Enter => {},
+        // if editing a note, save it
+        KeyCode::Char('s') => {}
+        // navigate up/down list of notes
+        KeyCode::Up => {}
+        KeyCode::Down => {}
+
+        // default case
+        _ => {}
+    }
+}
