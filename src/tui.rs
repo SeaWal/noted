90a5475f743560a6db4457a4801bf9b1a5545bfd
SeaWal <sean.walshe@tutanota.com>
2023-11-30 19:46:22 +0000

    Tui struct impl launches CrossTerm session

---
+++ src/tui.rs
@@ -0,0 +1,53 @@
+use anyhow::Result;
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{io, panic};
+
+pub type Terminal = ratatui::Terminal<ratatui::backend::CrosstermBackend<std::io::Stderr>>;
+
+use crate::{app::AppState, event::EventHandler, ui};
+
+pub struct Tui {
+    terminal: Terminal,
+    pub events: EventHandler,
+}
+
+impl Tui {
+    pub fn new(terminal: Terminal, events: EventHandler) -> Self {
+        Self { terminal, events }
+    }
+
+    pub fn enter(&mut self) -> Result<()> {
+        terminal::enable_raw_mode()?;
+        crossterm::execute!(io::stderr(), EnterAlternateScreen, EnableMouseCapture)?;
+
+        let panic_hook = panic::take_hook();
+        panic::set_hook(Box::new(move |panic| {
+            Self::reset().expect("failed to reset terminal");
+            panic_hook(panic);
+        }));
+
+        self.terminal.hide_cursor()?;
+        self.terminal.clear()?;
+        Ok(())
+    }
+
+    pub fn draw(&mut self, app: &mut AppState) -> Result<()> {
+        self.terminal.draw(|f| ui::render(app, f))?;
+        Ok(())
+    }
+
+    pub fn reset() -> Result<()> {
+        terminal::disable_raw_mode()?;
+        crossterm::execute!(io::stderr(), LeaveAlternateScreen, DisableMouseCapture)?;
+        Ok(())
+    }
+
+    pub fn exit(&mut self) -> Result<()> {
+        Self::reset()?;
+        self.terminal.show_cursor()?;
+        Ok(())
+    }
+}
