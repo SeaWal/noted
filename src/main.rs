54c0e504e33fc9ab2087227b824c6f9735110f0d
SeaWal <sean.walshe@tutanota.com>
2023-12-01 14:20:24 +0000

    implemented main fn

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,33 @@ pub mod tui;
 pub mod ui;
 pub mod update;
 
-fn main() {
-    
+use anyhow::Result;
+use app::AppState;
+use event::{EventHandler, EventType};
+use ratatui::{backend::CrosstermBackend, Terminal};
+use tui::Tui;
+use update::update;
+
+fn main() -> Result<()> {
+    let mut app = AppState::new();
+
+    let backend = CrosstermBackend::new(std::io::stderr());
+    let terminal = Terminal::new(backend)?;
+    let event_handler = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, event_handler);
+    tui.enter()?;
+
+    while !app.should_quit {
+        tui.draw(&mut app)?;
+
+        match tui.events.next()? {
+            EventType::Tick => {}
+            EventType::Key(key_event) => update(&mut app, key_event),
+            EventType::Mouse(_) => {}
+            EventType::Resize(_, _) => {}
+        };
+    }
+
+    tui.exit()?;
+    Ok(())
 }
