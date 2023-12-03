51560af1eed3be093827cc84f6723dad7b864553
SeaWal <sean.walshe@tutanota.com>
2023-12-03 19:24:49 +0000

    render Main and Editing views separately

--- src/ui.rs
+++ src/ui.rs
@@ -1,23 +1,24 @@
-use ratatui::style::{Color, Style};
-use ratatui::widgets::{Paragraph, Block, Borders, BorderType};
 use ratatui::prelude::{Alignment, Frame};
+use ratatui::style::{Color, Style};
+use ratatui::widgets::{Block, BorderType, Borders, Paragraph};
 
-use crate::app::AppState;
+use crate::app::{AppState, CurrentView};
 
-pub fn render(_app: &mut AppState, frame: &mut Frame) {
-    frame.render_widget(
-        Paragraph::new(
-            "Render notes here..."
-        )
-        .block(
-            Block::default()
-            .title("Noted")
-            .title_alignment(Alignment::Center)
-            .borders(Borders::ALL)
-            .border_type(BorderType::Rounded)
-        )
-        .style(Style::default().fg(Color::Yellow))
-        .alignment(Alignment::Center),
-        frame.size(),
-    );
+pub fn render(app: &mut AppState, frame: &mut Frame) {
+    match app.current_view {
+        CurrentView::Main => frame.render_widget(
+            Paragraph::new("Render notes here...")
+                .block(
+                    Block::default()
+                        .title("Noted")
+                        .title_alignment(Alignment::Center)
+                        .borders(Borders::ALL)
+                        .border_type(BorderType::Rounded),
+                )
+                .style(Style::default().fg(Color::Yellow))
+                .alignment(Alignment::Center),
+            frame.size(),
+        ),
+        CurrentView::Editing => {}
+    }
 }
