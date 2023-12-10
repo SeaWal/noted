186b17b8b944293043927ce8eac50e6093a8bb06
SeaWal <sean.walshe@tutanota.com>
2023-12-10 17:13:03 +0000

    added nav hints

--- src/ui.rs
+++ src/ui.rs
@@ -1,10 +1,21 @@
 use ratatui::prelude::{Alignment, Frame};
 use ratatui::style::{Color, Style};
+use ratatui::text::Span;
 use ratatui::widgets::{Block, BorderType, Borders, Paragraph};
 
 use crate::app::{AppState, CurrentView};
 
 pub fn render(app: &mut AppState, frame: &mut Frame) {
+    let nav_hints = {
+        match app.current_view {
+            CurrentView::Main => Span::styled("(q/Esc) to quit", Style::default().fg(Color::Red)),
+
+            CurrentView::Editing => {
+                Span::styled("(q/Esc) to quit", Style::default().fg(Color::Blue))
+            }
+        }
+    };
+
     match app.current_view {
         CurrentView::Main => frame.render_widget(
             Paragraph::new("Render notes here...")
