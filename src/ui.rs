42a09fa13dccf0da9fdde1461b30bae59e565669
SeaWal <sean.walshe@tutanota.com>
2023-12-11 18:52:57 +0000

    added vertical layout to main view

--- src/ui.rs
+++ src/ui.rs
@@ -1,11 +1,28 @@
+use ratatui::layout::{Constraint, Layout};
 use ratatui::prelude::{Alignment, Frame};
 use ratatui::style::{Color, Style};
-use ratatui::text::Span;
+use ratatui::text::{Span, Text, Line};
 use ratatui::widgets::{Block, BorderType, Borders, Paragraph};
 
 use crate::app::{AppState, CurrentView};
 
 pub fn render(app: &mut AppState, frame: &mut Frame) {
+    let layout = Layout::default()
+        .direction(ratatui::layout::Direction::Vertical)
+        .constraints([
+            Constraint::Length(3),
+            Constraint::Min(1),
+            Constraint::Length(3),
+        ])
+        .split(frame.size());
+
+    let title_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default());
+
+    let title = Paragraph::new(Text::styled("Noted", Style::default())).block(title_block);
+    frame.render_widget(title, layout[1]);
+
     let nav_hints = {
         match app.current_view {
             CurrentView::Main => Span::styled("(q/Esc) to quit", Style::default().fg(Color::Red)),
@@ -15,6 +32,8 @@ pub fn render(app: &mut AppState, frame: &mut Frame) {
             }
         }
     };
+
+    frame.render_widget(Paragraph::new(Line::from(nav_hints)), layout[2]);
 
     match app.current_view {
         CurrentView::Main => frame.render_widget(
