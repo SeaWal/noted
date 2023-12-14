e7cd6cc8fcd519f37e7b0eae7c73c318ffd46973
SeaWal <sean.walshe@tutanota.com>
2023-12-14 19:44:45 +0000

    render_notes is working now

--- src/ui.rs
+++ src/ui.rs
@@ -1,38 +1,31 @@
 use ratatui::layout::{Constraint, Layout};
 use ratatui::prelude::{Alignment, Frame};
-use ratatui::style::{Color, Style};
+use ratatui::style::{Color, Modifier, Style, Stylize};
 use ratatui::text::{Line, Span, Text};
-use ratatui::widgets::{Block, BorderType, Borders, Cell, List, ListItem, Paragraph, Row, Table};
+use ratatui::widgets::{Block, BorderType, Borders, Cell, Paragraph, Row, Table};
 
 use crate::app::{AppState, CurrentView};
-use crate::note::{self, NoteList};
+use crate::note::NoteList;
 
 pub fn render(app: &mut AppState, frame: &mut Frame) {
     let layout = Layout::default()
         .direction(ratatui::layout::Direction::Vertical)
         .constraints([
-            Constraint::Length(3),
+            Constraint::Length(1),
             Constraint::Min(1),
             Constraint::Length(3),
         ])
         .split(frame.size());
 
-    let title_block = Block::default()
-        .borders(Borders::ALL)
-        .style(Style::default());
-
-    let title = Paragraph::new(Text::styled("Noted", Style::default())).block(title_block);
-    frame.render_widget(title, layout[0]);
-
-    let list = render_notes2(&mut app.notes);
+    let list = render_notes(&mut app.notes);
     frame.render_widget(list, layout[1]);
 
     let nav_hints = {
         match app.current_view {
-            CurrentView::Main => Span::styled("((q/Esc) to quit", Style::default().fg(Color::Red)),
+            CurrentView::Main => Span::styled("((q/Esc) to quit", Style::default()),
 
             CurrentView::Editing => {
-                Span::styled("((q/Esc) to quit", Style::default().fg(Color::Blue))
+                Span::styled("((q/Esc) to quit", Style::default())
             }
         }
     };
@@ -41,7 +34,7 @@ pub fn render(app: &mut AppState, frame: &mut Frame) {
 
     match app.current_view {
         CurrentView::Main => frame.render_widget(
-            Paragraph::new("Render notes here...")
+            Paragraph::new("")
                 .block(
                     Block::default()
                         .title("Noted")
@@ -49,7 +42,7 @@ pub fn render(app: &mut AppState, frame: &mut Frame) {
                         .borders(Borders::ALL)
                         .border_type(BorderType::Rounded),
                 )
-                .style(Style::default().fg(Color::Yellow))
+                .style(Style::default())
                 .alignment(Alignment::Center),
             frame.size(),
         ),
@@ -57,38 +50,37 @@ pub fn render(app: &mut AppState, frame: &mut Frame) {
     }
 }
 
-fn render_notes<'a>(note_list: &mut NoteList) -> List<'a> {
-    let items: Vec<ListItem> = note_list
-        .iter()
-        .map(|note| ListItem::new(format!("{} {} {}", note.id, note.title, note.created_at)))
-        .collect();
-
-    let list = List::new(items).block(Block::default().title("Notes").borders(Borders::ALL));
-
-    list
-}
-
-fn render_notes2(note_list: &mut NoteList) -> Table<'_> {
+fn render_notes(note_list: &mut NoteList) -> Table<'_> {
     let rows: Vec<Row> = note_list
         .iter()
         .map(|note| {
             Row::new(vec![
-                Cell::from(Span::raw(note.id.to_string())),
-                Cell::from(Span::raw(note.title.clone())),
-                Cell::from("hello")
+                Cell::from(Span::from(note.id.to_string())),
+                Cell::from(Span::from(note.title.clone())),
+                Cell::from(Span::from(note.created_at.to_string())),
             ])
         })
         .collect();
 
-    let col_names = Row::new(vec![Cell::from("ID"), Cell::from("Title")]);
+    let col_names = Row::new(vec![
+        Cell::from("ID").add_modifier(Modifier::BOLD),
+        Cell::from("Title").add_modifier(Modifier::BOLD),
+        Cell::from("Created At").add_modifier(Modifier::BOLD),
+    ]);
 
-    let table = Table::new(rows.into_iter()).header(col_names).block(
-        Block::default()
-            .borders(Borders::ALL)
-            .style(Style::default().fg(Color::White))
-            .title("Detail")
-            .border_type(BorderType::Plain),
-    );
+    let table = Table::new(rows.into_iter())
+        .header(col_names)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default())
+                .border_type(BorderType::Plain),
+        )
+        .widths(&[
+            Constraint::Percentage(10),
+            Constraint::Percentage(40),
+            Constraint::Percentage(20),
+        ]);
 
     table
 }
