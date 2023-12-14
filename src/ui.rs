533b17eac292e6386b7742c5493bb1b102cbd344
SeaWal <sean.walshe@tutanota.com>
2023-12-14 19:31:52 +0000

    fn render_notes returns Table to render to TUI

--- src/ui.rs
+++ src/ui.rs
@@ -24,23 +24,15 @@ pub fn render(app: &mut AppState, frame: &mut Frame) {
     let title = Paragraph::new(Text::styled("Noted", Style::default())).block(title_block);
     frame.render_widget(title, layout[0]);
 
-    let mut list_items = Vec::<ListItem>::new();
-    for note in app.notes.iter() {
-        list_items.push(ListItem::new(Line::from(Span::styled(
-            format!("{: <10} Hello", note.id.to_string()),
-            Style::default().fg(Color::Green),
-        ))))
-    }
-
-    let list = List::new(list_items);
+    let list = render_notes2(&mut app.notes);
     frame.render_widget(list, layout[1]);
 
     let nav_hints = {
         match app.current_view {
-            CurrentView::Main => Span::styled("(q/Esc) to quit", Style::default().fg(Color::Red)),
+            CurrentView::Main => Span::styled("((q/Esc) to quit", Style::default().fg(Color::Red)),
 
             CurrentView::Editing => {
-                Span::styled("(q/Esc) to quit", Style::default().fg(Color::Blue))
+                Span::styled("((q/Esc) to quit", Style::default().fg(Color::Blue))
             }
         }
     };
@@ -65,24 +57,38 @@ pub fn render(app: &mut AppState, frame: &mut Frame) {
     }
 }
 
-fn render_notes<'a>(note_list: &mut NoteList) -> Table<'a> {
-    let notes = Block::default()
-        .borders(Borders::ALL)
-        .style(Style::default().fg(Color::White))
-        .border_type(BorderType::Plain);
+fn render_notes<'a>(note_list: &mut NoteList) -> List<'a> {
+    let items: Vec<ListItem> = note_list
+        .iter()
+        .map(|note| ListItem::new(format!("{} {} {}", note.id, note.title, note.created_at)))
+        .collect();
 
-    let details: Vec<_> = note_list
+    let list = List::new(items).block(Block::default().title("Notes").borders(Borders::ALL));
+
+    list
+}
+
+fn render_notes2(note_list: &mut NoteList) -> Table<'_> {
+    let rows: Vec<Row> = note_list
         .iter()
         .map(|note| {
             Row::new(vec![
-                Cell::from(Span::raw(note.id.into())),
-                Cell::from(Span::raw(note.title)),
-                Cell::from(Span::raw(note.created_at.into())),
+                Cell::from(Span::raw(note.id.to_string())),
+                Cell::from(Span::raw(note.title.clone())),
+                Cell::from("hello")
             ])
         })
         .collect();
 
-    let table = Table::new(details);
+    let col_names = Row::new(vec![Cell::from("ID"), Cell::from("Title")]);
+
+    let table = Table::new(rows.into_iter()).header(col_names).block(
+        Block::default()
+            .borders(Borders::ALL)
+            .style(Style::default().fg(Color::White))
+            .title("Detail")
+            .border_type(BorderType::Plain),
+    );
 
     table
 }
