e6db6452e70f3d4a0168648c22b84dd7b30ce259
SeaWal <sean.walshe@tutanota.com>
2024-02-11 14:39:39 +0000

    removed redundant fns

--- src/app.rs
+++ src/app.rs
@@ -59,57 +59,8 @@ impl AppState {
 
     pub fn set_current_note(&mut self) {
         match self.notes.get(self.current_note) {
-            Some(note) => note.set_content(&self.input_text),
+            Some(note) => note.set_content(&self.textbox.text),
             None => {}
         }
     }
-
-    pub fn inc_cursor(&mut self) {
-        if self.cursor_pos == self.input_text.len() - 1 {
-            return;
-        }
-        self.cursor_pos += 1;
-    }
-
-    pub fn dec_cursor(&mut self) {
-        if self.cursor_pos != 0 {
-            self.cursor_pos -= 1;
-        }
-    }
-
-    pub fn inc_line(&mut self) {
-        self.cursor_pos = match find_next_newline(self.input_text.clone(), self.cursor_pos) {
-            Some(index) => index + 1,
-            None => self.cursor_pos,
-        }
-    }
-
-    pub fn dec_line(&mut self) {
-        self.cursor_pos = match find_prev_newline(self.input_text.clone(), self.cursor_pos) {
-            Some(index) => index,
-            None => self.cursor_pos,
-        }
-    }
-}
-
-fn find_next_newline(input: String, start_index: usize) -> Option<usize> {
-    for (index, ch) in input.char_indices().skip(start_index) {
-        if ch == '\n' {
-            return Some(index);
-        }
-    }
-    None
-}
-
-fn find_prev_newline(input: String, start_index: usize) -> Option<usize> {
-    for (index, ch) in input
-        .char_indices()
-        .rev()
-        .skip_while(|&(i, _)| i > start_index-1)
-    {
-        if ch == '\n' {
-            return Some(index-1);
-        }
-    }
-    None
 }
